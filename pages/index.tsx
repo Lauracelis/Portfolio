f9aad484fb20f270a867557d42619b633b1f1f08
Laura Celis <lauracelis436@gmail.com>
2022-12-14 11:05:36 -0500

    Add: Installation Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head >
+        <title >Lauras Portfolio</title>
+      </Head>
+      <h1 className="text-blue-800">Hey</h1>
+    </div>
+  )
+}
+export default Home
